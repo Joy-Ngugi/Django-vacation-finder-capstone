b17449eef91bcd04b968d2ea465c2e2dde8c98a1
Joy Ngugi <ngugijoy51@gmail.com>
2025-02-10 20:03:57 +0300

    new

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,7 +16,7 @@ import os
 from decouple import Config
 import dj_database_url
 
-config = Config()
+config = Config(repository='.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
