4c5e31be60bcf218986234a3b89750af256e44f3
Joy Ngugi <ngugijoy51@gmail.com>
2025-02-10 19:31:10 +0300

    new

--- myproject/settings.py
+++ myproject/settings.py
@@ -179,10 +179,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")   
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+# EMAIL_HOST_USER = config("EMAIL_HOST_USER")   
+# EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+# DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 
+EMAIL_HOST_USER="joyngugi559@gmail.com"
+EMAIL_HOST_PASSWORD="fiim pymz lydy hoia"
+DEFAULT_FROM_EMAIL="Safiri Central Kenya <joyngugi559@gmail.com>"
 
 STRIPE_PUBLIC_KEY = config("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY")
